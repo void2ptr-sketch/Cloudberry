2fdaf6860930cc7f7117de9ca24dac12917bbfa7
Alexander Soloviev <a.a.so@yandex.ru>
2026-06-12 12:47:59 +0300

    [MODULE-FEDERATION]

--- src/app/remote/remote-shell.component.ts
+++ src/app/remote/remote-shell.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const CLOUDBERRY_THEME_BODY_CLASSES = [
+  'theme-prod',
+  'theme-dev',
+  'theme-test',
+  'theme-lt',
+  'theme-preprod',
+] as const;
 
 @Component({
   selector: 'app-cloudberry-remote-shell',
   imports: [RouterOutlet],
   template: '<router-outlet />',
-  styles: ':host { display: block; }',
+  styles: ':host { display: block; min-height: 0; }',
 })
-export class RemoteShellComponent {}
+export class RemoteShellComponent implements OnDestroy {
+  ngOnDestroy(): void {
+    document.body.classList.remove(...CLOUDBERRY_THEME_BODY_CLASSES);
+  }
+}
